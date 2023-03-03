7720556cb2c56068addab85d1c40c8871cc58cc7
Duckinic14 <125532900+Duckinic14@users.noreply.github.com>
2023-03-03 23:30:36 +0800

    Update prob001.py

--- prob001.py
+++ prob001.py
@@ -1,7 +1,7 @@
 
 sum = 0
 for i in range (1, 1000):
-  if i % 3 == 0  or i % 5 == 0: #factor of 3 or 5
-    sum += i
-print (sum) 
+  if i % 3 == 0  or i % 5 == 0: # factor of 3 or 5
+    sum += i 
+print (sum) # answer is 233168
 
